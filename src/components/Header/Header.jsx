14f4282339afc3076865e8e75dd6026c9890b24c
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-06-13 04:26:09 +0600

    google map added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Autocomplete } from "@react-google-maps/api";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
-import useStyles from "./styles.js";
+import useStyles from "./styles";
 
 const Header = () => {
   const classes = useStyles();
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import { Rating } from "@material-ui/lab";
+import useStyles from "./styles";
 
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
+  const coordinates = { lat: 0, lng: 0 };
   return (
-    <div>
-      <h1>Map</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyBnzfhbEIQ3cbzIWNMEhmIRyUtcHHmFL_0" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={15}
+        margin={(50, 50, 50, 50)}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
     </div>
   );
 };
